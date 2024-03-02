d250be5179feb8c007cac7dc49da61099e092f3c
Mikerosoft99 <kerosamy8000@gmail.com>
2024-03-02 19:06:44 +0200

    deleted action.xml and added them to other xml files

--- hms/__manifest__.py
+++ hms/__manifest__.py
@@ -6,11 +6,10 @@
     'description': """This module provides a comprehensive Hospital Management System (HMS) for managing various aspects of a hospital, including patients, doctors, appointments, and medical records. It includes features such as patient registration, appointment scheduling, medical history tracking, and billing.""",
     'depends': ['base'],
     'data': [
-        'views/actions.xml',
         'views/menu.xml',
         'views/patient.xml',
-        'views/doctor.xml',
         'views/department.xml',
+        'views/doctor.xml',
         'security/ir.model.access.csv',
     ],
     'application': True,
