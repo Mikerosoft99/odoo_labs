077e49fc176411bef02daf7a7a8e43c048205f1f
Mikerosoft99 <kerosamy8000@gmail.com>
2024-03-02 14:21:44 +0200

    Created directory for hms

---
+++ hms/models/hms.patient.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api
+
+class Patient(models.Model):
+    _name =
+    _description =
+    _rec_name =
+
+    first_name =
+    last_name =
+    birth_date =
+    history =
+    cr_ratio =
+    blood_type =
+    pcr =
+    image =
+    address =
+    age =
