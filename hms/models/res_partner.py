5a358ca0fc5db547e2ae2c980d2e88155f3379dd
Mikerosoft99 <kerosamy8000@gmail.com>
2024-03-06 20:21:58 +0200

    Added a model and xml for CRM inheritance

---
+++ hms/models/res_partner.py
@@ -0,0 +1,7 @@
+from odoo import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    related_patient_id = fields.Many2one('hms.patient', string='Related Patient', groups='base.group_user')
